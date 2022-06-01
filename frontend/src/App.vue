054cb02fd879f901bbbf06d59d88e6a24329f8a1
Ali Shefaee <shafaprince2020@gmail.com>
2022-06-02 00:09:49 +0430

    init PR

---
+++ frontend/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        const item = {
+            date: '2016-05-02',
+            name: 'Tom',
+            address: 'No. 189, Grove St, Los Angeles'
+        };
+        return {
+            tableData: Array(20).fill(item)
+        }
+    }
+};
+</script>
+<style>
+#app {
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*color: #2c3e50;*/
+}
+
+.el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+}
+
+.el-aside {
+    color: #333;
+}
+.bg-purple {
+    background: #d3dce6;
+}
+.bg-purple-light {
+    background: #e5e9f2;
+}
+.grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+}
+</style>
